9f32601c8553bab5a546e139c549c757420afb93
Minh Quang <minhquang2971999@gmail.com>
2019-06-19 01:49:45 +0700

    done

--- 1.js
+++ 1.js
@@ -68,3 +68,149 @@ $(window).scroll(function () {
         $(".mes5-scroll").removeClass("mes5-scroll").addClass("mes5");
     }
 });
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 2000) {
+        $(".lacmatem").removeClass("lacmatem").addClass("lacmatem1");
+    } else {
+        $(".lacmatem1").removeClass("lacmatem1").addClass("lacmatem");
+    }
+});
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 4300) {
+        $(".content").removeClass("content").addClass("content1");
+    } else {
+        $(".content1").removeClass("content1").addClass("content");
+    }
+});
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 5500) {
+        $(".mes6").removeClass("mes6").addClass("mes6-scroll");
+    } if (scroll <= 5000) {
+        $(".mes6-scroll").removeClass("mes6-scroll").addClass("mes6");
+    }
+});
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 5660) {
+        $(".mes7").removeClass("mes7").addClass("mes7-scroll");
+    } if (scroll < 5500) {
+        $(".mes7-scroll").removeClass("mes7-scroll").addClass("mes7");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 5900) {
+        $(".tinnhan1").removeClass("tinnhan1").addClass("tinnhan11");
+    } if (scroll < 5900) {
+        $(".tinnhan11").removeClass("tinnhan11").addClass("tinnhan1");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 6100) {
+        $(".tinnhan2").removeClass("tinnhan2").addClass("tinnhan21");
+    } if (scroll < 6100) {
+        $(".tinnhan21").removeClass("tinnhan21").addClass("tinnhan2");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 6469) {
+        $(".queto3").removeClass("queto3").addClass("queto31");
+    } if (scroll < 6469) {
+        $(".queto31").removeClass("queto31").addClass("queto3");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 7264) {
+        $(".anh2").removeClass("anh2").addClass("anh21");
+    } if (scroll < 7264) {
+        $(".anh21").removeClass("anh21").addClass("anh2");
+    }
+});
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 7400) {
+        $(".anh3").removeClass("anh3").addClass("anh31");
+    } if (scroll < 7400) {
+        $(".anh31").removeClass("anh31").addClass("anh3");
+    }
+});
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 8032) {
+        $(".content4").removeClass("content4").addClass("content41");
+    } if (scroll < 8032) {
+        $(".content41").removeClass("content41").addClass("content4");
+    }
+});
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 8400) {
+        $(".anh4").removeClass("anh4").addClass("anh41");
+    } if (scroll < 8400) {
+        $(".anh41").removeClass("anh41").addClass("anh4");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 8685) {
+        $(".tinnhan3").removeClass("tinnhan3").addClass("tinnhan31");
+    } if (scroll < 8685) {
+        $(".tinnhan31").removeClass("tinnhan31").addClass("tinnhan3");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 8685) {
+        $(".tinnhan4").removeClass("tinnhan4").addClass("tinnhan41");
+    } if (scroll < 8685) {
+        $(".tinnhan41").removeClass("tinnhan41").addClass("tinnhan4");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 9200) {
+        $(".tinnhan5").removeClass("tinnhan5").addClass("tinnhan51");
+    } if (scroll < 9200) {
+        $(".tinnhan51").removeClass("tinnhan51").addClass("tinnhan5");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10408) {
+        $(".tinnhan6").removeClass("tinnhan6").addClass("tinnhan61");
+    } if (scroll < 10408) {
+        $(".tinnhan61").removeClass("tinnhan61").addClass("tinnhan6");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10993) {
+        $(".ten1").removeClass("ten1").addClass("ten11");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10993) {
+        $(".ten2").removeClass("ten2").addClass("ten21");
+    }
+});
+
